4c49bd0800321e3acedf7ae3a082d38c5ecc507b
ericazpcalaca <ericazpc@gmail.com>
2023-06-28 10:39:35 -0700

    Adjusted the Register screen and put an click listener on the login activity

--- app/src/main/java/com/example/yoga_app/Login.java
+++ app/src/main/java/com/example/yoga_app/Login.java
@@ -2,13 +2,27 @@ package com.example.yoga_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.signUp);
+
+        //Open the Register activity
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Register.class));
+            }
+        });
     }
 }
