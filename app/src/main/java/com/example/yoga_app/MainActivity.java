3345eca1c17508f583303e685216d7923beaf735
ericazpcalaca <ericazpc@gmail.com>
2023-07-10 10:12:50 -0700

    Add a menu botton on top to open the nav

--- app/src/main/java/com/example/yoga_app/MainActivity.java
+++ app/src/main/java/com/example/yoga_app/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.yoga_app;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private FirebaseUser user;
     private androidx.appcompat.widget.Toolbar toolbar;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
 //        auth = FirebaseAuth.getInstance();
@@ -51,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
