5da73e7859c08bcfa3d9c2fe076f8fc9898a5792
ericazpcalaca <ericazpc@gmail.com>
2023-07-10 10:03:14 -0700

    Added the drawer menu

--- app/src/main/java/com/example/yoga_app/MainActivity.java
+++ app/src/main/java/com/example/yoga_app/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,31 +18,38 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogout;
     private TextView textView;
     private FirebaseUser user;
+    private androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        btnLogout = findViewById(R.id.btnLogout);
-        textView = findViewById(R.id.textView);
-        user = auth.getCurrentUser();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        if(user == null){
-            startActivity(new Intent(getApplicationContext(),Login.class));
-            finish();
-        }else{
-            textView.setText(user.getEmail());
-        }
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),Login.class));
-                finish();
-            }
-        });
+//        auth = FirebaseAuth.getInstance();
+//        btnLogout = findViewById(R.id.btnLogout);
+//        textView = findViewById(R.id.textView);
+//        user = auth.getCurrentUser();
+//
+//        if(user == null){
+//            startActivity(new Intent(getApplicationContext(),Login.class));
+//            finish();
+//        }else{
+//            textView.setText(user.getEmail());
+//        }
+//
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(getApplicationContext(),Login.class));
+//                finish();
+//            }
+//        });
+
+
     }
 }
