6daf12bea0095c9d73be40e8f37ebfc2e3bda930
ericazpcalaca <ericazpc@gmail.com>
2023-07-10 10:50:45 -0700

    Added the fragments on the menu items

--- app/src/main/java/com/example/yoga_app/MainActivity.java
+++ app/src/main/java/com/example/yoga_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yoga_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -8,15 +9,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth auth;
     private Button btnLogout;
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseUser user;
     private androidx.appcompat.widget.Toolbar toolbar;
     private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +39,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Home()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
 
 
 //        auth = FirebaseAuth.getInstance();
@@ -70,5 +84,31 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Home()).commit();
+                break;
+            case R.id.explore:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Explore()).commit();
+                break;
+            case R.id.progress:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new DailyProgress()).commit();
+                break;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Profile()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this,"Share",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this,"Send",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
     }
 }
