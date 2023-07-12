125e4ed2be7767c78fdd0b0a6ee5ea49b0cdadda
ericazpcalaca <ericazpc@gmail.com>
2023-07-12 10:51:01 -0700

    Created the Pose Library activity

--- app/src/main/java/com/example/yoga_app/Home.java
+++ app/src/main/java/com/example/yoga_app/Home.java
@@ -1,5 +1,6 @@
 package com.example.yoga_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,26 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class Home extends Fragment {
-    private Button buttonOpenActivity;
+    private ImageButton btnPoseLibrary;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-//        buttonOpenActivity = view.findViewById(R.id.buttonOpenActivity);
-//        buttonOpenActivity.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Handle button click event
-//                openActivity();
-//            }
-//        });
+        btnPoseLibrary = view.findViewById(R.id.btnPoseLibrary);
+        btnPoseLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click event
+                openActivity();
+            }
+        });
 
-        // Inflate the layout for this fragment
         return view;
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(getActivity(), PoseLibrary.class);
+        startActivity(intent);
     }
 }
