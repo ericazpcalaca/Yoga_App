1ac3e622cb2c3a37fc3b630a7995e41c6ce210d0
ericazpcalaca <ericazpc@gmail.com>
2023-08-04 13:59:57 -0700

    Started to put the json file on the SelectedWorkout

--- app/src/main/java/com/example/yoga_app/SelectedWorkout.java
+++ app/src/main/java/com/example/yoga_app/SelectedWorkout.java
@@ -6,23 +6,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class SelectedWorkout extends AppCompatActivity {
 
     private int typeofWorkout;
     private TextView workOutTitle;
+    private TextView workOutDesc;
+    private ArrayList<YogaPose> poseList;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_workout);
 
-        Intent intent = getIntent();
-        typeofWorkout = intent.getIntExtra("type",-1);
 
         workOutTitle = findViewById(R.id.workOutTitle);
-        workOutTitle.setText("O work out eh " + typeofWorkout);
+        workOutDesc = findViewById(R.id.txtDescWorkout);
+
+        Intent intent = getIntent();
+        typeofWorkout = intent.getIntExtra("type", -1);
+
+        requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
+        fetchYogaWorkout(typeofWorkout);
 
 
+        poseList = new ArrayList<>();
+
+    }
+
+    private void fetchYogaWorkout(int poseNumber) {
+        String url = "https://yoga-api-nzy4.onrender.com/v1/categories";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, response -> {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(poseNumber);
+                        String categoryName = jsonObject.getString("category_name");
+                        String categoryDescription = jsonObject.getString("category_description");
+
+                        workOutTitle.setText(categoryName);
+                        workOutDesc.setText(categoryDescription);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO: Handle error
+                        Toast.makeText(SelectedWorkout.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+        requestQueue.add(jsonArrayRequest);
     }
 }
