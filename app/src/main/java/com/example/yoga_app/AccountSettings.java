d23cc88304522129de832481d4e58699c7692507
ericazpcalaca <ericazpc@gmail.com>
2023-08-09 21:03:54 -0700

    Adjust the account settings

--- app/src/main/java/com/example/yoga_app/AccountSettings.java
+++ app/src/main/java/com/example/yoga_app/AccountSettings.java
@@ -3,12 +3,25 @@ package com.example.yoga_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AccountSettings extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+
+        toolbar = findViewById(R.id.toolbarAccountSettings);
+        //Set a back to main page button on top
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_ios_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
