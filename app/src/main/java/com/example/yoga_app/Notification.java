84f9d22287bf825aee47564aa2288c3349c924c8
ericazpcalaca <ericazpc@gmail.com>
2023-08-16 11:27:14 -0700

    Added Notifications

--- app/src/main/java/com/example/yoga_app/Notification.java
+++ app/src/main/java/com/example/yoga_app/Notification.java
@@ -1,7 +1,18 @@
 package com.example.yoga_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,12 +39,54 @@ public class Notification extends AppCompatActivity {
             }
         });
 
+
         btnNotification = findViewById(R.id.btnSetNotification);
         btnNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                askPermission();
+                makeNotification();
             }
         });
+    }
+
+    private void askPermission() {
+        //Ask for the permission if needed
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(ContextCompat.checkSelfPermission(Notification.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(Notification.this, new String[]{Manifest.permission.POST_NOTIFICATIONS},101);
+            }
+        }
+    }
+
+    public void makeNotification(){
+        String channelID = "CHANNEL_ID_NOTIFICATION";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),channelID);
+        builder.setSmallIcon(R.drawable.yogaapp)
+                .setContentTitle("Are you ready do do some exercises?")
+                .setContentText("Tap to start now!")
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,PendingIntent.FLAG_MUTABLE);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = notificationManager.getNotificationChannel(channelID);
+            if (notificationChannel == null){
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                notificationChannel = new NotificationChannel(channelID,"Yoga",importance);
+                notificationChannel.enableVibration(true);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+        notificationManager.notify(0, builder.build());
     }
 }
